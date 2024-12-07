aea4cde856c26ed69230908b882eb0cc7e6038fb
Rahul092002 <sinha7rahul@gmail.com>
2024-12-07 03:31:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let userName = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* Primitive datatypes in javascript
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+*/
+
+// object (also a user defined datatype)
+
+//typeof is used to know the datatype of variable
+console.log(typeof userName);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
