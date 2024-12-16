4d58f46c4c63d22157537174bb84990685ac92dc
Rahul092002 <sinha7rahul@gmail.com>
2024-12-16 07:27:02 +0000

    scopes basic

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,59 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) //This will not throw error as addone is function name 
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) //this will throw error as addtwo is a variable name 
+//          which has stored function not a function name itself
+const addTwo = function(num){
+    return num + 2
+}
