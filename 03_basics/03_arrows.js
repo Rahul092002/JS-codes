830e7014c5a69a6a594d3d49605a05546f6fecab
Rahul Sinha <sinha7rahul@gmail.com>
2025-01-18 10:52:27 +0000

    arrow basics done

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -3,6 +3,7 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
+        //to refer current context we use this keyword here, current context =  "hitesh"
         console.log(`${this.username} , welcome to website`);
         console.log(this);
     }
@@ -11,6 +12,22 @@ const user = {
 
 // user.welcomeMessage()
 // user.username = "sam"
-user.welcomeMessage()
+//user.welcomeMessage()
 
 //console.log(this);    This shows this contains empty object if not defined inside a object
+//                    But if you run console.log(this); in browser this will show you 'window' as global object
+ function chai(){
+    console.log(this.username) //will show undeifined
+ }
+
+ //chai()
+
+//const addTwo = (num1,num2) => { //syntax of arrow function
+//    return num1 + num2
+//}
+
+//const addTwo = (num1,num2) => (num1+num2) //This is the syntax for implicit return 
+//                                            where only one line of return statement is done
+//we can also pass object using '()' in arrow funtion
+const addTwo = (num1,num2) => ({username:"hitesh"})
+console.log(addTwo())
