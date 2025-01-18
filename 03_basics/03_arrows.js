1b4b483c98041163a1e8dd97868622491afb51ef
Rahul Sinha <sinha7rahul@gmail.com>
2025-01-18 08:25:28 +0000

    arrow concepts

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,16 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+user.welcomeMessage()
+
+//console.log(this);    This shows this contains empty object if not defined inside a object
